15fe613e7db3f52b93ea6e086de0abd5631f68c3
Austin Rooks <austin@r00ks.io>
2025-02-19 22:12:44 -0600

    add way to read back instructions

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -11,9 +11,9 @@ pub type Opcode = u8;
 pub const OP_CONSTANT: Opcode = 0;
 
 #[derive(Clone)]
-struct Definition {
-    name: &'static str,
-    operand_widths: Vec<u16>,
+pub struct Definition {
+    pub name: &'static str,
+    pub operand_widths: Vec<u16>,
 }
 
 const DEFINITIONS: LazyCell<HashMap<Opcode, Definition>> = LazyCell::new(|| {
@@ -45,13 +45,13 @@ pub fn make(op: &Opcode, operands: impl IntoIterator<Item = u16>) -> Vec<u8> {
         let mut instruction = vec![0u8; instruction_len.into()];
         instruction[0] = *op;
 
-        let mut offset = 1u16;
+        let mut offset = 1;
         for (i, o) in operands.into_iter().enumerate() {
             let width = def.operand_widths[i];
             match width {
                 2 => {
                     let mut cursor = Cursor::new(&mut instruction);
-                    cursor.seek_relative(offset.into()).unwrap();
+                    cursor.seek_relative(offset as i64).unwrap();
                     cursor.write(&o.to_be_bytes()).unwrap();
                 }
                 _ => todo!(),
@@ -62,6 +62,61 @@ pub fn make(op: &Opcode, operands: impl IntoIterator<Item = u16>) -> Vec<u8> {
         instruction
     } else {
         vec![]
+    }
+}
+
+fn read_u16(ins: Vec<u8>) -> u16 {
+    let arr: [u8; 2] = ins.try_into().unwrap();
+    u16::from_be_bytes(arr)
+}
+
+pub fn read_operands(def: &Definition, ins: Instructions) -> (Vec<u16>, usize) {
+    let mut operands = vec![0u16; def.operand_widths.len()];
+    let mut offset = 0;
+
+    for (i, width) in def.operand_widths.iter().enumerate() {
+        match width {
+            2 => operands[i] = read_u16(ins[offset..offset + *width as usize].to_vec()),
+            _ => todo!(),
+        };
+
+        offset += *width as usize;
+    }
+
+    (operands, offset.into())
+}
+
+fn instruction_to_string(ins: &Instructions) -> String {
+    let mut out = String::new();
+
+    let mut i = 0;
+    while i < ins.len() {
+        let def = look_up(&ins[i]).unwrap();
+
+        let (operands, read) = read_operands(&def, ins[i + 1..].to_vec());
+
+        out.push_str(&format!(
+            "{:04} {}\n",
+            i,
+            format_instruction(&def, operands).unwrap()
+        ));
+
+        i += 1 + read;
+    }
+
+    out
+}
+
+fn format_instruction(def: &Definition, operands: Vec<u16>) -> Result<String, String> {
+    let operand_count = def.operand_widths.len();
+
+    if operands.len() != operand_count {
+        return Err("operand len doesn't match defined len.".to_string());
+    }
+
+    match operand_count {
+        1 => Ok(format!("{} {}", def.name, operands[0])),
+        _ => todo!(),
     }
 }
 
@@ -90,6 +145,55 @@ mod test {
 
             for (i, _) in test.expected.iter().enumerate() {
                 assert_eq!(instruction[i], test.expected[i]);
+            }
+        }
+    }
+
+    #[test]
+    fn test_instructions_string() {
+        let instructions: Vec<Instructions> = vec![
+            make(&OP_CONSTANT, vec![1]),
+            make(&OP_CONSTANT, vec![2]),
+            make(&OP_CONSTANT, vec![65535]),
+        ];
+
+        let expected = r#"0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+"#;
+
+        let concated = instructions
+            .iter()
+            .flatten()
+            .map(|v| v.to_owned())
+            .collect::<Vec<_>>();
+
+        assert_eq!(expected, instruction_to_string(&concated));
+    }
+
+    #[test]
+    fn test_read_operands() {
+        struct Test {
+            op: Opcode,
+            operands: Vec<u16>,
+            bytes_read: usize,
+        }
+
+        let tests = vec![Test {
+            op: OP_CONSTANT,
+            operands: vec![65535],
+            bytes_read: 2,
+        }];
+
+        for t in tests.iter() {
+            let instruction = make(&t.op, t.operands.clone());
+            let def = look_up(&t.op).unwrap();
+
+            let (operands_read, n) = read_operands(&def, instruction[1..].to_vec());
+            assert_eq!(n, t.bytes_read);
+
+            for (i, want) in t.operands.iter().enumerate() {
+                assert_eq!(operands_read[i], *want);
             }
         }
     }
